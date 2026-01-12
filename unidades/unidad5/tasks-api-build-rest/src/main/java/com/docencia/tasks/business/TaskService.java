e16a665a5e0ba8faaae7b972ea47c421c7ebeb79
danielrguezh <danielrh111005@gmail.com>
2026-01-12 17:14:13 +0000

    task estructura hexagonal

---
+++ unidades/unidad5/tasks-api-build-rest/src/main/java/com/docencia/tasks/business/TaskService.java
@@ -0,0 +1,48 @@
+package com.docencia.tasks.business;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.docencia.tasks.domain.model.Task;
+import com.docencia.tasks.repository.TaskRepository;
+
+@Service
+public class TaskService implements ITaskService{
+    private final TaskRepository repo;
+
+    public TaskService(TaskRepository repo){
+        this.repo=repo;
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createTask'");
+    }
+
+    @Override
+    public List<Task> getTasks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTasks'");
+    }
+
+    @Override
+    public Task getTaskById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTaskById'");
+    }
+
+    @Override
+    public Task updateTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateTask'");
+    }
+
+    @Override
+    public boolean deleteTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteTask'");
+    }
+
+}
